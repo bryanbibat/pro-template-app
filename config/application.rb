994d265b0d67d60acd9fc2c0d2aeb420117bb4fe
bry <bry@bryanbibat.net>
2011-01-03 21:06:27 +0800

    login screen

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,9 @@ module ProTemplateApp
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
+    config.to_prepare do
+      Devise::SessionsController.layout "sign"
+    end
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
   end
