5ca228f7178234715502baeaa0df6344a7e6949e
bry <bry@bryanbibat.net>
2011-01-03 19:56:13 +0800

    initial commit, with .gitignore changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProTemplateApp::Application.config.secret_token = 'ff69afae8bd96c709e5b740f66edeb6560b5db7828832b64b218ef8c2f3d1b9f1513eccd7b456b625314ca7b71c8759cacf184d791e9b8d204bbe298ae25e664'
